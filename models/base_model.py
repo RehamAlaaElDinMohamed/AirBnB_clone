203a02b2079d2fd83b6a38b9cc07ff74001461fa
ahmedfoad <eng.ahmedfoad@gmail.com>
2023-08-12 23:59:35 +0300

    Create Modle files & Test Cases

---
+++ models/base_model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module contains a base class called
+'BaseModel'that defines all common
+attributes/methods for other classes.
+"""
+
+import uuid
+from datetime import datetime
+from models import storage
+
+
+class BaseModel:
+    pass
